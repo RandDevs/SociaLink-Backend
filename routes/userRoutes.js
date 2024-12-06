8d8bd273065d237609382b74aac0e03f5109676c
Nexvaulus <moehammad.fikr@gmail.com>
2024-12-07 06:48:38 +0700

    Refactor user routes for improved readability; change getUserProfile to use GET method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,14 @@
 import express from "express";
-import {
-  createUserProfile,
-  uploadProfilePicture,
-  uploadBanner,
-  getNotifications,
-  getUserProfile,
-  handleFollow,
-  handleUnfollow,
-  handleDeleteAccount,
-  getNewestUser,
-} from "../controllers/userController.js";
+import { createUserProfile, uploadProfilePicture, uploadBanner, getNotifications, getUserProfile, handleFollow, handleUnfollow, handleDeleteAccount, getNewestUser } from "../controllers/userController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
-import {
-  uploadProfilePict,
-  uploadBanner as bannerUpload,
-} from "../middleware/uploadMiddleware.js";
+import { uploadProfilePict, uploadBanner as bannerUpload } from "../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
 // Create user profile route
 router.post("/createUserProfile", authenticateToken, createUserProfile);
 
-router.post(
-  "/upload-profile-pict",
-  uploadProfilePict.single("profilePict"),
-  uploadProfilePicture
-);
+router.post("/upload-profile-pict", uploadProfilePict.single("profilePict"), uploadProfilePicture);
 
 router.post("/upload-banner", bannerUpload.single("banner"), uploadBanner);
 
@@ -36,7 +19,7 @@ router.get("/get/users", getNewestUser);
 router.post("/get/notifications", authenticateToken, getNotifications);
 
 // User profile page
-router.post("/get/userProfile", getUserProfile);
+router.get("/get/userProfile", getUserProfile);
 
 // handle follow
 router.post("/:userId/follow/:targetUserId", handleFollow);
